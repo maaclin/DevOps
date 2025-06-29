2b2cab3e7b5dd0f35aa41009b996e62ca44b413d
maaclin <123658689+maaclin@users.noreply.github.com>
2025-06-29 02:10:03 +0100

    Add files via upload

---
+++ 2. BASH/1_basic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter number 1: " num1
+read -p "Enter number 2: " num2
+
+echo "Sum: $((num1+num2))"
+echo "Difference: $((num1-num2))"
+echo "Product: $((num1*num2))"
+echo "Quotient: $((num1/num2))"
